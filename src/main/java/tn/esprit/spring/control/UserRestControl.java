a36cbebf5b29a88ec9b6db68023bf0f4b8409cc6
HAMMICHE Adel <hammiche.adel@esprit.tn>
2026-03-05 00:23:46 +0100

    remove commentary sonarqube

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -1,10 +1,16 @@
 package tn.esprit.spring.control;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import tn.esprit.spring.entities.User;
 import tn.esprit.spring.services.IUserService;
@@ -22,7 +28,6 @@ public class UserRestControl {
 	@GetMapping("/retrieve-all-users")
 	public List<User> retrieveAllUsers() {
 		return userService.retrieveAllUsers();
-		//return list;
 	}
  
 	// http://localhost:????/timesheet-devops/retrieve-user/{user-id}
@@ -55,5 +60,6 @@ public class UserRestControl {
 		return userService.updateUser(user);
 	}
 	 
+
 } 
  
